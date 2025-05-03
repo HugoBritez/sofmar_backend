e445128f91574e982fb6372f90f845d3ee011431
HugoBritez <sebastianbritezdevelop@gmail.com>
2025-05-03 10:53:53 -0300

    feat: ahora el workflow puede actualizar la db de los clientes

--- src/DB/update.sql
+++ src/DB/update.sql
@@ -30,9 +30,55 @@ CREATE TABLE IF NOT EXISTS inventario_auxiliar_items (
 );
 
 -- Agregar columnas a inventario_auxiliar si no existen
-ALTER TABLE inventario_auxiliar ADD COLUMN IF NOT EXISTS autorizado int not null default 0;
-ALTER TABLE inventario_auxiliar ADD COLUMN IF NOT EXISTS fecha_cierre date;
-ALTER TABLE inventario_auxiliar ADD COLUMN IF NOT EXISTS hora_cierre time;
+SET @dbname = DATABASE();
+SET @tablename = "inventario_auxiliar";
+SET @columnname = "autorizado";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column autorizado already exists'",
+  "ALTER TABLE inventario_auxiliar ADD COLUMN autorizado int not null default 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "fecha_cierre";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column fecha_cierre already exists'",
+  "ALTER TABLE inventario_auxiliar ADD COLUMN fecha_cierre date"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "hora_cierre";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column hora_cierre already exists'",
+  "ALTER TABLE inventario_auxiliar ADD COLUMN hora_cierre time"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
 
 -- Creación de la tabla agenda_subvisitas si no existe
 CREATE TABLE IF NOT EXISTS agenda_subvisitas (
@@ -61,13 +107,88 @@ UPDATE menu_sistemas SET m_descripcion = 'Informe planificaciones' WHERE m_grupo
 UPDATE menu_sistemas SET m_descripcion = 'Ingreso de planificación' WHERE m_grupo = 2 AND m_orden = 18;
 
 -- Agregar columnas a detalle_compras si no existen
-ALTER TABLE detalle_compras ADD COLUMN IF NOT EXISTS dc_cantidad_verificada INT NOT NULL DEFAULT 0;
+SET @tablename = "detalle_compras";
+SET @columnname = "dc_cantidad_verificada";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column dc_cantidad_verificada already exists'",
+  "ALTER TABLE detalle_compras ADD COLUMN dc_cantidad_verificada INT NOT NULL DEFAULT 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
 
 -- Agregar columnas a compras si no existen
-ALTER TABLE compras ADD COLUMN IF NOT EXISTS co_verificado INT NOT NULL DEFAULT 0;
-ALTER TABLE compras ADD COLUMN IF NOT EXISTS co_responsable_ubicacion INT NOT NULL DEFAULT 0;
-ALTER TABLE compras ADD COLUMN IF NOT EXISTS co_verificador INT NOT NULL DEFAULT 0;
-ALTER TABLE compras ADD COLUMN IF NOT EXISTS co_confirmador INT NOT NULL DEFAULT 0;
+SET @tablename = "compras";
+SET @columnname = "co_verificado";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column co_verificado already exists'",
+  "ALTER TABLE compras ADD COLUMN co_verificado INT NOT NULL DEFAULT 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "co_responsable_ubicacion";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column co_responsable_ubicacion already exists'",
+  "ALTER TABLE compras ADD COLUMN co_responsable_ubicacion INT NOT NULL DEFAULT 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "co_verificador";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column co_verificador already exists'",
+  "ALTER TABLE compras ADD COLUMN co_verificador INT NOT NULL DEFAULT 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "co_confirmador";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column co_confirmador already exists'",
+  "ALTER TABLE compras ADD COLUMN co_confirmador INT NOT NULL DEFAULT 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
 
 -- Creación de la tabla configuraciones_web si no existe
 CREATE TABLE IF NOT EXISTS `configuraciones_web` (
@@ -84,10 +205,70 @@ INSERT IGNORE INTO configuraciones_web (descripcion, valor) VALUES
     ('Ajustes Factura', '{}');
 
 -- Agregar columnas a pedidos si no existen
-ALTER TABLE pedidos ADD COLUMN IF NOT EXISTS p_imprimir_preparacion int not null default 0;
-ALTER TABLE pedidos ADD COLUMN IF NOT EXISTS p_preparado_por int not null default 0;
-ALTER TABLE pedidos ADD COLUMN IF NOT EXISTS p_cantidad_cajas int not null default 0;
-ALTER TABLE pedidos ADD COLUMN IF NOT EXISTS p_verificado_por int not null default 0;
+SET @tablename = "pedidos";
+SET @columnname = "p_imprimir_preparacion";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column p_imprimir_preparacion already exists'",
+  "ALTER TABLE pedidos ADD COLUMN p_imprimir_preparacion int not null default 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "p_preparado_por";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column p_preparado_por already exists'",
+  "ALTER TABLE pedidos ADD COLUMN p_preparado_por int not null default 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "p_cantidad_cajas";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column p_cantidad_cajas already exists'",
+  "ALTER TABLE pedidos ADD COLUMN p_cantidad_cajas int not null default 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
+
+SET @columnname = "p_verificado_por";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 'Column p_verificado_por already exists'",
+  "ALTER TABLE pedidos ADD COLUMN p_verificado_por int not null default 0"
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
 
 -- Creación de la tabla direcciones si no existe
 CREATE TABLE IF NOT EXISTS direcciones (
